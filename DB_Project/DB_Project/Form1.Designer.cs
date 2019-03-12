be4dcb56e47b0c70636dcf72c398c7a6a55fa8e9
2016CS101 <2016cs101@student.uet.edu.pk>
2019-03-12 04:06:58 -0700

    Dashboard Created

--- DB_Project/DB_Project/Form1.Designer.cs
+++ DB_Project/DB_Project/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DB_Project
 {
-    partial class Form1
+    partial class frmSplash
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DB_Project.Properties.Resources.imgSplash;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(976, 558);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(974, 553);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmSplash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Welcome";
+            this.Load += new System.EventHandler(this.frmSplash_Load);
+            this.Shown += new System.EventHandler(this.frmSplash_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- DB_Project/DB_Project/Form1.cs
+++ DB_Project/DB_Project/Form1.cs
@@ -7,14 +7,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DB_Project.Forms;
 
 namespace DB_Project
 {
-    public partial class Form1 : Form
+    public partial class frmSplash : Form
     {
-        public Form1()
+        public frmSplash()
         {
             InitializeComponent();
+        }
+        Timer tmr;
+        private void frmSplash_Shown(object sender, EventArgs e)
+        {
+            // Remve Border Of Splash Screen
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ControlBox = false;
+            this.Text = "";
+
+            tmr = new Timer();
+
+            //set time interval 3 sec
+
+            tmr.Interval = 3000;
+
+            //starts the timer
+
+            tmr.Start();
+
+            tmr.Tick += tmr_Tick;
+
+        }
+
+        void tmr_Tick(object sender, EventArgs e)
+
+        {
+
+            //after 3 sec stop the timer
+
+            tmr.Stop();
+
+            //display mainform
+
+            frmDashboard mf = new frmDashboard();
+
+            mf.Show();
+
+            //hide this form
+
+            this.Hide();
+
+            
+
+        }
+
+        private void frmSplash_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
